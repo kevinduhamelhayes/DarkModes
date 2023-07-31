ac5b3fb1b93e2e90aaf9490ed08b07484f318ac6
Kevin Duhamel Hayes <kevinduhamelh@gmail.com>
2023-07-31 20:54:54 -0300

    second commit+

--- DarkModes/src/components/Body.jsx
+++ DarkModes/src/components/Body.jsx
@@ -4,24 +4,33 @@ const Body = () => {
     <main className="body">
       <div className="body-container">
         <div className="body-container-left">
+          <figure>
+            <img src="./public/profile.jpeg" alt="" />
+          </figure>
           <h1 className="body-container-left-h1">
             Hi, I'm <span className="body-container-left-h1-span">Kevin</span>
           </h1>
           <p className="body-container-left-p">I'm a web developer</p>
           <p className="body-container-left-p">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur
-            corrupti adipisci quibusdam fugiat ipsa totam facilis placeat
-            architecto alias? Culpa, rem temporibus assumenda repudiandae sunt
-            consequatur itaque commodi voluptatibus architecto necessitatibus
-            minima a! Quae deserunt sit molestiae accusantium nam odit dolor vel
-            quam at repudiandae!
+            As a motivated Frontend Developer with a strong foundation in Python
+            Backend development, I have a passion for creating engaging and
+            user-friendly web experiences. My primary focus lies in mastering
+            JavaScript, React, and Tailwind CSS technologies to build responsive
+            and dynamic web applications. I am constantly seeking opportunities
+            to learn and apply new concepts to improve my skills. My journey
+            from backend development to frontend has given me a unique
+            perspective, making me adaptable and eager to take on new
+            challenges.
           </p>
         </div>
         <div className="body-container-right">
-          <h2 className="doby-h2">Lorem ipsum dolor sit.</h2>
+          <h2 className="doby-h2">Location</h2>
           <p className="body-container-right">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            Consequuntur, rem sequi. Accusantium.
+          I am currently based in Argentina.
+          </p>
+          <h2 className="doby-h2">Job Search</h2>
+          <p className="body-container-right">
+          I am actively seeking remote job opportunities.
           </p>
           <h2 className="doby-h2">Lorem ipsum dolor sit.</h2>
           <p className="body-container-right">
@@ -33,15 +42,11 @@ const Body = () => {
             Lorem ipsum dolor sit, amet consectetur adipisicing elit.
             Consequuntur, rem sequi. Accusantium.
           </p>
-          <h2 className="doby-h2">Lorem ipsum dolor sit.</h2>
+          <h2 className="doby-h2">hobbies</h2>
           <p className="body-container-right">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            Consequuntur, rem sequi. Accusantium.
-          </p>
-          <h2 className="doby-h2">Lorem ipsum dolor sit.</h2>
-          <p className="body-container-right">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            Consequuntur, rem sequi. Accusantium.
+            Reading tech blogs and articles Attending tech meetups and
+            conferences Practicing coding challenges and algorithms Exploring
+            new technologies and frameworks
           </p>
         </div>
       </div>
