0de7c53626911869e7d2127d020b9b295fc54c16
Kevin Duhamel Hayes <kevinduhamelh@gmail.com>
2023-08-05 00:59:02 -0300

    hi vercel

--- DarkModes/src/components/Footer.jsx
+++ DarkModes/src/components/Footer.jsx
@@ -7,12 +7,24 @@ const Footer = () => {
         <ul className="ul-footer-left">
           <li className="li-footer-left">Home</li>
           <li className="li-footer-left">About</li>
-          <li className="li-footer-left">Contact</li>
+          <li className="li-footer-left">
+            {" "}
+            <a
+              href="mailto:kevinduhamelh@gmail.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{
+                display: "block",
+                textDecoration: "none",
+                color: "inherit",
+              }}
+            >
+              Contact
+            </a>
+          </li>
         </ul>
       </div>
-      <div className="footer-right">
-        lovingly created by Kevin Duhamel
-      </div>
+      <div className="footer-right">lovingly created by Kevin Duhamel</div>
     </footer>
   )
 }
